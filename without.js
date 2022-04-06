e12a4e525ba789713104d9308adb3944b2796027
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-06 01:52:37 +0000

    updated lotide work

---
+++ without.js
@@ -0,0 +1,39 @@
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(arOne, arTwo) {
+  if (eqArrays(arOne, arTwo) === true) {
+    console.log(`✅✅✅Assertion Passed: ${arOne} ===  ${arTwo}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${arOne} !== ${arTwo}`);
+  }
+}; 
+
+
+let without = function(source, itemsToRemove) {
+  let newItems = [];
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.indexOf(source[i]) < 0) {
+      newItems.push(source[i]);
+    }
+  }
+  return newItems;
+}
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]) // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"])
+
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
