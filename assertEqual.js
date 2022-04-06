ab204f232f334133b0c8c6e39eb0243263c97ac9
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-06 00:57:09 +0000

    updated lotide work

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
   } else {
-    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual("banana", "banana");
+assertEqual(2, 1);
