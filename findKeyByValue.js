af5eeb8eb1ae4e1c657db9dbcfec3e32ee36269d
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-06 23:48:41 +0000

    lotide work

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,1 +1,27 @@
-hello
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  for (let genre of Object.keys(obj)) {
+    if (obj[genre] === value) {
+      return genre;
+    }
+  }
+};
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
