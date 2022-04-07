9fa7003d5b2397231b49fb5cab66102e6d564222
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-07 01:13:24 +0000

    lotide work

--- eqObjects.js
+++ eqObjects.js
@@ -6,3 +6,46 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+let eqArrays = function(arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (let keys of Object.keys(object1)) {
+      if (Array.isArray(object1[keys])) {
+        return eqArrays(object1[keys], object2[keys]);
+      } else {
+        if (object1[keys] !== object2[keys]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba),true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
